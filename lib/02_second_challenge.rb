7e9ece39af988f663baa1013da0df48b7602d3a1
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-09 13:05:19 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,11 +5,11 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }}  
+      }}
 end
