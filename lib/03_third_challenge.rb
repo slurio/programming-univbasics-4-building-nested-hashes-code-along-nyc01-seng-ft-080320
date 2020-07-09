ceaa1ea5922979385ea97d30f7b90240b2591b31
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-09 13:12:34 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,10 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-      :name => "Lord Montaue",:age => "53"
+      :name => "Lord Montague",:age => "53"
         },
       :matriarch => {
-        :name => "Lady Montaue",:age => "54"
+        :name => "Lady Montague",:age => "54"
         },
       :hero => {
         :name => "Romeo",:age => "15",:status => "alive"
